c1d96166ac9a51a8e8e3aab527650bba7a0d212f
lim0n3 <simonestaffa96@gmail.com>
2023-11-04 14:23:47 +0000

    Peanut protocol integration

---
+++ lib/peanut/index.ts
@@ -0,0 +1,44 @@
+import {Signer} from "ethers";
+import peanut from "@squirrel-labs/peanut-sdk";
+
+
+const CHAINID = 1442 // Polygon zkEVM Testnet
+
+export enum CourseTokenID {
+    WEB3_INTRODUCTION = 1,
+    EOA_SMART_ACCOUNTS = 2
+}
+
+/**
+ * Generate a peanut link to claim the course completion NFT and return it to the user (to claim it)
+ * @param signer - threelingo wallet, owner of the ERC1155 contract
+ * @param tokenAddress - address of the ERC1155 contract
+ * @param tokenId - id of the token (0 = Web3 Introduction, 1 = EOA and Smart Accounts)
+ */
+export const getPeanutLink = async (signer: Signer, tokenId: CourseTokenID) => {
+    return await peanut.createLink({
+        structSigner: {
+            signer
+        },
+        linkDetails: {
+            chainId: CHAINID,
+            tokenAddress: process.env.COURSE_TOKEN_ADDRESS,
+            tokenAmount: 1,
+            tokenType: 3,
+            tokenId
+        }
+    });
+}
+
+/**
+ * Claim a peanut link and send the token to the recipient address
+ * @param link - valid peanut link to claim, obtained through getPeanutLink
+ * @param recipientAddress - address where to send the tokens
+ */
+export const claimPeanutLinkGasless = async (link: string, recipientAddress: string) => {
+    return await peanut.claimLinkGasless({
+        link,
+        recipientAddress,
+        APIKey: process.env.NEXT_PUBLIC_PEANUT_API_KEY as string,
+    })
+}
